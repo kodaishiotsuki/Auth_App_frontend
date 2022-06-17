e9827736029bfe9928c6fb145df0461449b23492
kodaishiotsuki <0711dr5@gmail.com>
2022-06-17 17:41:54 +0900

    restAPI(login)取得

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  //初期状態設定
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  //ログインフォーム
+  const formSubmit = (e) => {
+    e.preventDefault();
+    //API取得(login)
+    const data = {
+      email: email,
+      password: password,
+    };
+    axios
+      .post("/login", data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <br />
@@ -10,13 +35,16 @@ const Login = () => {
         <div className='jumbotron col-lg-4 offset-lg-4'>
           <h3 className='text-center'>Login Account</h3>
 
-          <form>
+          <form onSubmit={formSubmit}>
             <div className='form-group'>
               <label for='exampleInputEmail1'>Email address</label>
               <input
                 type='email'
                 className='form-control'
                 aria-describedby='emailHelp'
+                name='email'
+                required
+                onChange={(e) => setEmail(e.target.value)}
               />
               <small id='emailHelp' className='form-text text-muted'>
                 We'll never share your email with anyone else.
@@ -24,14 +52,19 @@ const Login = () => {
             </div>
             <div className='form-group'>
               <label for='exampleInputPassword1'>Password</label>
-              <input type='password' className='form-control' />
+              <input
+                type='password'
+                className='form-control'
+                name='password'
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className='form-group form-check'></div>
             <button type='submit' className='btn btn-primary btn-block'>
               Login
             </button>
             <br />
-            
             Forget My Password <Link to='/forget'>Click Here</Link>
           </form>
         </div>
