e2ee8aa9d883007be91659a456ef930aaa36cfd2
kodaishiotsuki <0711dr5@gmail.com>
2022-06-18 20:48:31 +0900

    logout

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({ user }) => {
+const Navbar = ({ user, setUser }) => {
   // console.log(user);
+
+  //ログアウトボタンクリック
+  const handleLogout = () => {
+    localStorage.clear();
+    setUser(null);
+  };
+
   return (
     <div>
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
@@ -28,23 +35,39 @@ const Navbar = ({ user }) => {
               </Link>
             </li>
             <li className='nav-item'>
-              <Link className='nav-link' to='/profile'>
-                Profile
-              </Link>
+              {localStorage.getItem("token") && (
+                <Link className='nav-link' to='/profile'>
+                  Profile
+                </Link>
+              )}
             </li>
           </ul>
           <span className='navbar-text'>
             <ul className='navbar-nav mr-auto'>
-              <li className='nav-item'>
-                <Link className='nav-link' to='/login'>
-                  Login
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' to='/register'>
-                  Register
-                </Link>
-              </li>
+              {localStorage.getItem("token") ? (
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link'
+                    to='/'
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </Link>
+                </li>
+              ) : (
+                <>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='/login'>
+                      Login
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='/register'>
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </span>
         </div>
