f8612eadf53839ed208e38b21bf8b90cfbb09fb3
kodaishiotsuki <0711dr5@gmail.com>
2022-06-19 16:31:51 +0900

    URL保護

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = ({ user }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      return navigate("/login");
+    }
+  });
+
   return (
-    <div>
-      <br />
-      <br />
-      <div className='row'>
-        <div className='jumbotron col-lg-4 offset-lg-4'>
-          <h3 className='text-center'>User Profile</h3>
+    <>
+      <div>
+        <br />
+        <br />
+        <div className='row'>
+          <div className='jumbotron col-lg-4 offset-lg-4'>
+            <h3 className='text-center'>User Profile</h3>
 
-          <ul class='list-group'>
-            <li className='list-group-item'>Name : {user.name} </li>
-            <li className='list-group-item'>Email : {user.email}</li>
-          </ul>
-
+            <ul class='list-group'>
+              <li className='list-group-item'>Name : {user.name} </li>
+              <li className='list-group-item'>Email : {user.email}</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
